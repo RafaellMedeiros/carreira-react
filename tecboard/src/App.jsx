3837b2344d141356492557f391261ec28eb40d74
Rafael Medeiros <64844045+RafaellMedeiros@users.noreply.github.com>
2026-04-14 02:30:10 +0000

    feat: refactor event form component and improve structure

--- tecboard/src/App.jsx
+++ tecboard/src/App.jsx
@@ -1,11 +1,14 @@
+import "./App.css";
 
-import './App.css'
+function TituloFormulario({ children }) {
+  return <h2>{children}</h2>;
+}
 
 function FormularioDeEvento() {
   return (
     <form action="/">
-      <h2>Preencha para criar um evento:</h2>
-      <br/>
+      <TituloFormulario>Preencha para criar um evento:</TituloFormulario>
+      <br />
       <fieldset>
         <label htmlFor="name">Qual o nome do evento?</label>
         <input type="text" id="name" name="name" />
@@ -14,14 +17,12 @@ function FormularioDeEvento() {
         <label htmlFor="tema">Tema do evento</label>
         <input type="text" id="tema" name="tema" />
 
-        <br/>
+        <br />
         <input type="submit" value="Criar Evento" />
       </fieldset>
-
     </form>
-  )
+  );
 }
-
 
 function App() {
   return (
@@ -36,7 +37,7 @@ function App() {
         <FormularioDeEvento />
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
