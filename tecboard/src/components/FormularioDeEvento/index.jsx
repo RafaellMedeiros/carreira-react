ce2c567367ed45f6057d2e2bab1a00d3d01e42df
Rafael Medeiros <rafaelfufcg@gmail.com>
2026-04-17 22:32:23 -0300

    feat: update event form to use select for theme and improve button styling

--- tecboard/src/components/FormularioDeEvento/index.jsx
+++ tecboard/src/components/FormularioDeEvento/index.jsx
@@ -19,10 +19,15 @@ export function FormularioDeEvento() {
         <Label htmlFor="date">Data do evento</Label>
         <input className="campo-input" type="date" id="date" name="date" />
         <Label htmlFor="tema">Tema do evento</Label>
-        <input className="campo-input" type="text" id="tema" name="tema" />
-
-        <br />
-        <input type="submit" value="Criar Evento" />
+        {/* <input className="campo-input" type="text" id="tema" name="tema" /> */}
+        <select className="campo-select" id="tema" name="tema">
+          <option className="opcoes" value="">Selecione um tema</option>
+          <option className="opcoes" value="tecnologia">Tecnologia</option>
+          <option className="opcoes" value="saude">Saúde</option>
+          <option className="opcoes" value="educacao">Educação</option>
+          <option className="opcoes" value="cultura">Cultura</option>
+        </select>
+        <button type="submit" className="campo-botao">Criar evento</button>
       </fieldset>
     </form>
   );
