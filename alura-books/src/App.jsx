0d7ed62d7c37e6d1b469884c4f279e2a255b0bbc
Rafael Medeiros <rafaelfufcg@gmail.com>
2026-05-26 22:05:57 -0300

    chore: initialize project dependencies and node_modules structure

---
+++ alura-books/src/App.jsx
@@ -0,0 +1,18 @@
+import logo from '../public/logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="logo">
+          <img src={logo} alt="Logo Alura Books" />
+          <p>Alura Books</p>
+        </div>
+      </header>
+    </div>
+  )
+}
+
+export default App
