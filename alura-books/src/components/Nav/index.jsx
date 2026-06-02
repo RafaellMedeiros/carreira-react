e6676d083ab976a755280fba2aac09f20319cc5a
Rafael Medeiros <64844045+RafaellMedeiros@users.noreply.github.com>
2026-06-02 01:29:16 +0000

    refactor: update Nav component to use styled-components for layout

--- alura-books/src/components/Nav/index.jsx
+++ alura-books/src/components/Nav/index.jsx
@@ -1,37 +1,45 @@
-import './styles.css';
+import "./styles.css";
 
-import perfil from '../../assets/perfil.svg';
-import sacola from '../../assets/sacola.svg';
+import perfil from "../../assets/perfil.svg";
+import sacola from "../../assets/sacola.svg";
 
+import styled from "styled-components";
+
+const Icones = styled.ul`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+`;
 
 const menuItems = [
-    { name: "Home", link: "#home" },
-    { name: "Books", link: "#books" },
-    { name: "About", link: "#about" },
-  ];
+  { name: "Home", link: "#home" },
+  { name: "Books", link: "#books" },
+  { name: "About", link: "#about" },
+];
 
-  const menuIcons = [
-    { name: "Books", icon: sacola },
-    { name: "About", icon: perfil },
-  ];
+const menuIcons = [
+  { name: "Books", icon: sacola },
+  { name: "About", icon: perfil },
+];
 
 export default function Nav() {
-    return (
-        <nav>
-            <ul>
-                {menuItems.map((item) => (
-                    <li key={item.name}>
-                        <a href={item.link}>{item.name}</a>
-                    </li>
-                ))}
-            </ul>
-            <ul>
-                {menuIcons.map((item) => (
-                    <li className="icon" key={item.name}>
-                        <img src={item.icon} alt={item.name} />
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <Icones>
+        {menuItems.map((item) => (
+          <li key={item.name}>
+            <a href={item.link}>{item.name}</a>
+          </li>
+        ))}
+      </Icones>
+      <Icones>
+        {menuIcons.map((item) => (
+          <li className="icon" key={item.name}>
+            <img src={item.icon} alt={item.name} />
+          </li>
+        ))}
+      </Icones>
+    </nav>
+  );
 }
